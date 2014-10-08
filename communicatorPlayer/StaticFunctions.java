11b90d6470ef26ac0ceece5bc19803bd68e23fc9
robertonl <roberto.noorduyn.londono@gmail.com>
2014-10-08 16:07:52 +0200

    Update StaticFunctions.java
    just added a static random object here

--- communicatorPlayer/StaticFunctions.java
+++ communicatorPlayer/StaticFunctions.java
@@ -1,9 +1,11 @@
 package communicator;
 
+import java.util.Random;
 import battlecode.common.*;
 
 public class StaticFunctions {
 
+	public static Random rand = new Random(); 
 	
 	public static int locToInt(MapLocation m){
 		return (m.x*100 + m.y);
